973f6f9d5ae3e3b4d64bd16859b9fcc460a460d5
Jhanavi <janvisingh05192@gmail.com>
2021-06-05 00:11:11 +0530

    add changes

--- server.js
+++ server.js
@@ -3,22 +3,21 @@ const exphbs = require('express-handlebars');
 
 const app = express();
 
-app.engine('handlebars',exphbs({
-	defaultLayout : 'main'
+app.engine('handlebars', exphbs({
+    defaultLayout: 'main'
 }));
 
-app.set('view engine','handlebars');
+app.set('view engine', 'handlebars');
 
 app.use(express.static('public'));
 
 const port = process.env.PORT || 3000;
 
-app.get('/', (req , res) => {
-	res.render('home');
+app.get('/', (req, res) => {
+    res.render('home');
 });
 
 
-
-app.listen(port , () => {
-	console.log(`Server is running on port ${port}`);
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
